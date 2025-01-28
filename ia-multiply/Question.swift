b054d0ac2df395d1f81a4b1a3a445d81e2946927
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2025-01-28 09:19:16 -0800

    Update Question.swift

--- ia-multiply/Question.swift
+++ ia-multiply/Question.swift
@@ -1,10 +1,11 @@
+
 import Foundation
 
 class Question {
     var factor0 = Int()
     var factor1 = Int()
     var equation: String
-
+    var answer: String
     init() {
 //        let time = UInt32(NSDate().timeIntervalSinceReferenceDate)
 //        srand(time)
@@ -12,5 +13,6 @@ class Question {
         factor0 = Int.random(in: 1..<10)
         factor1 = Int.random(in: 1..<10)
         equation = "\(factor0) X \(factor1)"
+        answer = String(factor0*factor1)
     }
 }
