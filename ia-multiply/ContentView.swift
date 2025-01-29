712d1add55178e1d644a358c8d3e48a3422fd0ef
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2025-01-29 09:15:02 -0800

    Update ContentView.swift

--- ia-multiply/ContentView.swift
+++ ia-multiply/ContentView.swift
@@ -2,33 +2,13 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @State private var input: String = ""
-    @State private var userInput: String = ""
-    @State var currentQuestion = Question();
-    @State var errorMessage = ""
     var body: some View {
-        Text(currentQuestion.equation)
-        Text(errorMessage)
-        TextField(
-            "Enter Answer",
-            text: $input
-        ).disableAutocorrection(true)
-        .border(.secondary)
-        .keyboardType(.numberPad)
-        .padding()
-        .frame(width: 200)
-        .onChange(of: input) {
-            if (!input.isEmpty ){
-                if (input == currentQuestion.answer){
-                    currentQuestion = Question()
-                    errorMessage = "Correct!\nTry this one:"
-                    input = ""
-                } else if(input.count == currentQuestion.answer.count){
-                    errorMessage = "Incorrect.\nTry again."
-                }
+        NavigationView {
+            NavigationLink {
+                DetailView()
+            } label: {
+                Text("Detail View")
+            }
             }
         }
-        
     }
-}
-
