df706d58043ff066fdcc5822c75882a2c1464223
EllisWaterman <111319501+EllisWaterman@users.noreply.github.com>
2025-01-30 09:25:13 -0800

    Update DetailView.swift

--- ia-multiply/DetailView.swift
+++ ia-multiply/DetailView.swift
@@ -7,38 +7,59 @@
 import SwiftUI
 
 struct DetailView: View {
-        @State var input: String = ""
-        @State var userInput: String = ""
-        @State var currentQuestion = Question();
-        @State var errorMessage = ""
-        @State var numQuestions = Int()
+    @State var input: String = ""
+    @State var userInput: String = ""
+    @State var currentQuestion = Question();
+    @State var errorMessage = ""
+    @State var numQuestions = Int()
+    var delay: TimeInterval = 1.0
+    @State var isActive = false
+
+
     var body: some View {
-        Text(currentQuestion.equation)
-        Text(errorMessage)
-        TextField(
-            "Enter Answer",
-            text: $input
-        ).disableAutocorrection(true)
-            .border(.secondary)
-            .keyboardType(.numberPad)
-            .padding()
-            .frame(width: 200)
-            .onChange(of: input) {
-                if (!input.isEmpty ){
-                    if (input == currentQuestion.answer){
-                        currentQuestion = Question()
-                        errorMessage = "Correct!\nTry this one:"
-                        input = ""
-                    } else if(input.count == currentQuestion.answer.count){
-                        errorMessage = "Incorrect.\nTry again."
+        NavigationStack {
+            VStack {
+                navigationDestination(isPresented: $isActive) {
+                    ContentView()
+                }
+                Text(errorMessage)
+                    .frame(maxWidth: .infinity)
+                    .font(.system(size: 30))
+                Text(currentQuestion.equation)
+                
+                TextField(
+                    "Enter Answer",
+                    text: $input
+                )
+                .disableAutocorrection(true)
+                .border(.secondary)
+                .keyboardType(.numberPad)
+                .padding()
+                .frame(width: 200)
+                .onChange(of: input) {
+                    
+                    if(numQuestions >= 10) {
+                        isActive = true
+                    } else if (!input.isEmpty){
+                        if (input == currentQuestion.answer){
+                            currentQuestion = Question()
+                            errorMessage = "Correct!\nTry this one:"
+                            input = ""
+                            DispatchQueue.main.asyncAfter(deadline: .now()+delay) {
+                                errorMessage = ""
+                            }
+                            numQuestions += 1
+                        } else if(input.count == currentQuestion.answer.count){
+                            errorMessage = "Incorrect.\nTry again."
+                            DispatchQueue.main.asyncAfter(deadline: .now()+delay) {
+                                errorMessage = ""
+                            }
+                            
+                        }
+                        
                     }
                 }
-            }        
-    }
-}
-
-struct DetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        DetailView()
+            }
+        }
     }
 }
